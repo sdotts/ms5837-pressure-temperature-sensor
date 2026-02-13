829bb023d20a3ea273d15eb67ee354d667cb79bc
sdotts <157402512+sdotts@users.noreply.github.com>
2026-02-12 18:43:00 -0700

    Continue Development

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
-enum SENS_MODEL {
-        //% block="02BA"
-        MOD_02BA = 0x0,
-        //% block="30BA"
-        MOD_30BA = 0x1
-}
-
+// Port of MS5837 Arduinio Library From Blue Robotics To MakeCode
 
 
 /**
@@ -12,20 +6,76 @@ enum SENS_MODEL {
  */
 //% weight=100 color=#70c0f0 icon="\uf773" block="MS5837"
 namespace MS5837 {
-    let MS5837_I2C_ADDR = 0x76; //sensor address
-    let SENSOR_MODEL = SENS_MODEL.MOD_02BA; //Set Defualt Model
-    let PROM: number[] = []     //Prom Calibration
-    
-    function reset():void {
-        pins.i2cWriteNumber(MS5837_I2C_ADDR, 0x1E, NumberFormat.UInt8BE)
+    //Addresses and commands
+    const MS5837_I2C_ADDR = 0x76;
+    const MS5837_RESET_CMD = 0x1E;
+    const MS5837_ADC_READ = 0x00;
+    const MS5837_PROM_READ = 0xA0;
+    const MS5837_CONVERT_D1_8192 = 0x4A;
+    const MS5837_CONVERT_D2_8192 = 0x5A;
+
+    //Sensor Models
+    enum SENS_MODEL {
+        //% block="02BA"
+        MOD_02BA = 0x0,
+        //% block="30BA"
+        MOD_30BA = 0x1
     }
 
-    function read_PROM():void {
+    //Common Densities
+    enum waterDensity {
+        //% block="Fresh Water"
+        FRESH = 997,
+        //% block="Salt Water"
+        SALT = 1029
+    }
+
+    //Defualt Values and Variables
+    let fluidDensity = waterDensity.FRESH; //kg/m^3 for fresh water at 25C
+    let SENSOR_MODEL = SENS_MODEL.MOD_02BA; //Set Defualt Model
+    let PROM: number[] = []     //Prom Calibration
+
+
+    function reset(): void {
+        pins.i2cWriteNumber(MS5837_I2C_ADDR, MS5837_RESET_CMD, NumberFormat.UInt8BE)
+    }
+
+    function crc4(prom: number[]): number {
+        let nRem = 0
+        let nProm = prom.slice(0)
+        nProm[0] = nProm[0] & 0x0FFF
+        nProm[7] = 0
+
+        for (let i = 0; i < 16; i++) {
+            if (i % 2 == 1) {
+                nRem ^= nProm[i >> 1] & 0x00FF
+            } else {
+                nRem ^= nProm[i >> 1] >> 8
+            }
+            for (let nBit = 8; nBit > 0; nBit--) {
+                if (nRem & 0x8000) {
+                    nRem = (nRem << 1) ^ 0x3000
+                } else {
+                    nRem = nRem << 1
+                }
+            }
+        }
+
+        nRem = (nRem >> 12) & 0x000F
+        return nRem
+    }
+
+
+    function read_PROM(): void {
         PROM = []
         for (let i = 0; i < 7; i++) {
-            pins.i2cWriteNumber(MS5837_I2C_ADDR, 0xA0 + i * 2, NumberFormat.UInt8BE)
+            pins.i2cWriteNumber(MS5837_I2C_ADDR, MS5837_PROM_READ + i * 2, NumberFormat.UInt8BE)
             let value = pins.i2cReadNumber(MS5837_I2C_ADDR, NumberFormat.UInt16BE)
             PROM.push(value)
+        }
+        let crccalc = crc4(PROM)
+        if (crccalc != (PROM[0] >> 12)) {
+            control.panic(55) //CRC Error
         }
     }
 
@@ -35,6 +85,7 @@ namespace MS5837 {
     //% block="initialize MS5837"
     export function init(): void {
         reset()
+        basic.pause(10)
         read_PROM()
     }
 
@@ -43,145 +94,8 @@ namespace MS5837 {
      */
     //% blockId="MS5837_SET_MODEL" block="set address %model"
     //% weight=50 blockGap=8
-    export function Address(model: SENS_MODEL) {
+    export function model(model: SENS_MODEL) {
         SENSOR_MODEL = model
     }
 
 }
-
-
-
-
-
-
-
-
-// /**
-//  * makecode BMP280 digital pressure sensor Package.
-//  * From microbit/micropython Chinese community.
-//  * http://www.micropython.org.cn
-//  */
-
-// enum BMP280_I2C_ADDRESS {
-//     //% block="0x76"
-//     ADDR_0x76 = 0x76,
-//     //% block="0x77"
-//     ADDR_0x77 = 0x77
-// }
-/**
- * BMP280 block
- */
-//% weight=100 color=#70c0f0 icon="\uf042" block="BMP280"
-// namespace BMP280 {
-//     let BMP280_I2C_ADDR = BMP280_I2C_ADDRESS.ADDR_0x76
-
-//     function reset
-
-//     function setreg(reg: number, dat: number): void {
-//         let buf = pins.createBuffer(2);
-//         buf[0] = reg;
-//         buf[1] = dat;
-//         pins.i2cWriteBuffer(BMP280_I2C_ADDR, buf);
-//     }
-
-//     function getreg(reg: number): number {
-//         pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
-//         return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.UInt8BE);
-//     }
-
-//     function getUInt16LE(reg: number): number {
-//         pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
-//         return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.UInt16LE);
-//     }
-
-//     function getInt16LE(reg: number): number {
-//         pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
-//         return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.Int16LE);
-//     }
-
-//     let dig_T1 = getUInt16LE(0x88)
-//     let dig_T2 = getInt16LE(0x8A)
-//     let dig_T3 = getInt16LE(0x8C)
-//     let dig_P1 = getUInt16LE(0x8E)
-//     let dig_P2 = getInt16LE(0x90)
-//     let dig_P3 = getInt16LE(0x92)
-//     let dig_P4 = getInt16LE(0x94)
-//     let dig_P5 = getInt16LE(0x96)
-//     let dig_P6 = getInt16LE(0x98)
-//     let dig_P7 = getInt16LE(0x9A)
-//     let dig_P8 = getInt16LE(0x9C)
-//     let dig_P9 = getInt16LE(0x9E)
-//     setreg(0xF4, 0x2F)
-//     setreg(0xF5, 0x0C)
-//     let T = 0
-//     let P = 0
-
-//     function get(): void {
-//         let adc_T = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
-//         let var1 = (((adc_T >> 3) - (dig_T1 << 1)) * dig_T2) >> 11
-//         let var2 = (((((adc_T >> 4) - dig_T1) * ((adc_T >> 4) - dig_T1)) >> 12) * dig_T3) >> 14
-//         let t = var1 + var2
-//         T = Math.idiv(((t * 5 + 128) >> 8), 100)
-//         var1 = (t >> 1) - 64000
-//         var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * dig_P6
-//         var2 = var2 + ((var1 * dig_P5) << 1)
-//         var2 = (var2 >> 2) + (dig_P4 << 16)
-//         var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((dig_P2) * var1) >> 1)) >> 18
-//         var1 = ((32768 + var1) * dig_P1) >> 15
-//         if (var1 == 0)
-//             return; // avoid exception caused by division by zero
-//         let adc_P = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
-//         let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
-//         _p = Math.idiv(_p, var1) * 2;
-//         var1 = (dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
-//         var2 = (((_p >> 2)) * dig_P8) >> 13
-//         P = _p + ((var1 + var2 + dig_P7) >> 4)
-//     }
-
-//     /**
-//      * get pressure
-//      */
-//     //% blockId="BMP280_GET_PRESSURE" block="get pressure"
-//     //% weight=80 blockGap=8
-//     export function pressure(): number {
-//         get();
-//         return P;
-//     }
-
-//     /**
-//      * get temperature
-//      */
-//     //% blockId="BMP280_GET_TEMPERATURE" block="get temperature"
-//     //% weight=80 blockGap=8
-//     export function temperature(): number {
-//         get();
-//         return T;
-//     }
-
-//     /**
-//      * power on
-//      */
-//     //% blockId="BMP280_POWER_ON" block="Power On"
-//     //% weight=61 blockGap=8
-//     export function PowerOn() {
-//         setreg(0xF4, 0x2F)
-//     }
-
-//     /**
-//      * power off
-//      */
-//     //% blockId="BMP280_POWER_OFF" block="Power Off"
-//     //% weight=60 blockGap=8
-//     export function PowerOff() {
-//         setreg(0xF4, 0)
-//     }
-
-    // /**
-    //  * set I2C address
-    //  */
-    // //% blockId="BMP280_SET_ADDRESS" block="set address %addr"
-    // //% weight=50 blockGap=8
-    // export function Address(addr: BMP280_I2C_ADDRESS) {
-    //     BMP280_I2C_ADDR = addr
-    // }
-// }
